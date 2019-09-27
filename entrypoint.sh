bde91f7178f858367cc1da593bc55d6fb1514526
Bo Yao <bo@nearprotocol.com>
2019-09-27 12:17:06 -0700

    entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,8 +13,8 @@ git checkout FETCH_HEAD
 branch="${GITHUB_REF}"
 git checkout -b "${branch}"
 
-sh -c "git remote add mirror https://${GITLAB_USERNAME}:${GITLAB_PASSWORD}@${GITLAB_HOSTNAME}/$*"
-sh -c "echo pushing to ${branch} branch at $(git remote get-url --push mirror)"
+sh -c "git remote add mirror https://${GITLAB_USERNAME}:$(/cred_helper.sh)@${GITLAB_HOSTNAME}/$*"
+# sh -c "echo pushing to ${branch} branch at $(git remote get-url --push mirror)"
 sh -c "git push mirror ${branch}"
 
 sleep $POLL_TIMEOUT
